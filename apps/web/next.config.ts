045b6736f6a9b0eb6bc8c1c7d7f3c2e5e556acd2
Geetansh <90083776+Geetanshgarg@users.noreply.github.com>
2026-03-27 21:22:11 +0530

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@AmaZon-Clone/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
